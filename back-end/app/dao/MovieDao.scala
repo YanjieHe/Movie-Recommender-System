db160c2f7d88f0d125ce4112d12c06e1997ffec4
Yanjie He <heyanjie0@outlook.com>
2019-07-26 16:07:10 -0400

    Update MovieDao.scala

--- back-end/app/dao/MovieDao.scala
+++ back-end/app/dao/MovieDao.scala
@@ -1,57 +1,51 @@
 package dao
 
-import javax.inject.{Inject, Provider}
-import play.api.MarkerContext
-import scala.concurrent.{ExecutionContext, Future}
-import play.api.libs.json._
 import scala.annotation.tailrec
-
 import java.sql.DriverManager
 import java.sql.Connection
 import java.sql.ResultSet
 
 import model.Movie
 
-class MovieResourceHandler {
+class MovieResourceHandler extends ResourceHandler {
   def read(imdbId: Int): Option[Movie] = {
-    val driver = "com.mysql.jdbc.Driver"
-    val url = "jdbc:mysql://localhost/mydb"
-    val username = "root"
-    val password = "123456"
 
     var connection: Connection = null
     var result: Option[Movie] = None
 
     try {
-      // make the connection
-      Class.forName(driver)
-      connection = DriverManager.getConnection(url, username, password)
-
-      // create the statement, and run the select query
+      connection = connect()
       val statement = connection.createStatement()
-      val resultSet =
-        statement.executeQuery(
-          s"SELECT IMDB_ID, Primary_Title, Original_Title, Avg_Rating, Num_Votes, Start_Year, Runtime_Minutes, Poster_Link FROM Movies WHERE IMDB_ID = $imdbId;"
-        )
+      val columns = List(
+        "IMDB_ID",
+        "Primary_Title",
+        "Original_Title",
+        "Avg_Rating",
+        "Num_Votes",
+        "Start_Year",
+        "Runtime_Minutes",
+        "Poster_Link"
+      ).mkString(", ")
+      val resultSet = statement.executeQuery(
+        s"SELECT $columns FROM Movies WHERE IMDB_ID = $imdbId;"
+      )
       result = Some(
         collect(
           resultSet,
           resultSet => {
-            def get(colName: String): String = {
-              resultSet.getString(colName)
-            }
+            val col: String => String = resultSet.getString
             Movie(
-              get("IMDB_ID").toInt,
-              get("Primary_Title"),
-              get("Original_Title"),
-              get("Avg_Rating").toFloat,
-              get("Num_Votes").toInt,
-              get("Start_Year") match {
+              col("IMDB_ID").toInt,
+              col("Primary_Title"),
+              col("Original_Title"),
+              col("Avg_Rating").toFloat,
+              col("Num_Votes").toInt,
+              col("Start_Year") match {
                 case null => None
                 case s    => Some(s.toInt)
               },
-              get("Runtime_Minutes").toInt,
-              get("Poster_Link") match {
+              col("Runtime_Minutes").toInt,
+              col("Poster_Link") match {
                 case null => None
                 case link => Some(link)
               }
@@ -64,17 +58,5 @@ class MovieResourceHandler {
     }
     connection.close()
     result
-  }
-
-  def collect[A](resultSet: ResultSet, converter: ResultSet => A): List[A] = {
-    @tailrec
-    def iter(result: List[A]): List[A] = {
-      if (resultSet.next()) {
-        iter(converter(resultSet) :: result)
-      } else {
-        result
-      }
-    }
-    iter(Nil).reverse
   }
 }
